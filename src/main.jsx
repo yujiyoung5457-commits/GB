bf7b053efad527af144094876727b9ea9b1209e6
yujiyoung5457 <yujiyoung5457@gmail.com>
2026-07-14 17:19:04 +0900

    브라우저 동일

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+// 수정: 앱 스타일보다 먼저 공통 브라우저 리셋을 적용합니다.
+import './styles/reset.css'
 import './index.css'
 import App from './App.jsx'
 
